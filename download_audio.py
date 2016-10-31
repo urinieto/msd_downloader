822b1f73fb443649c0109b66bfb06692cfae6da5
Oriol Nieto <oriol.nieto@gmail.com>
2016-10-30 19:24:57 -0700

    Added option of downloading files, one by one

--- download_audio.py
+++ download_audio.py
@@ -5,9 +5,17 @@ Downloads the MSD preview tracks in parallel.
 import argparse
 from joblib import Parallel, delayed
 import logging
+import pandas as pd
 import os
 import subprocess
 import time
+import urllib
+
+
+def ensure_dir(directory):
+    """Makes sure that the given directory exists."""
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 
 def download_subfolder(url, index):
@@ -16,10 +24,35 @@ def download_subfolder(url, index):
     subprocess.call(["wget", "-r", "-nH", sub_url])
 
 
-def process(url, n_jobs):
-    N = 26
-    return Parallel(n_jobs=n_jobs)(delayed(download_subfolder)(
-        url, i) for i in range(N))
+def download_file(audio_path, url, out_dir):
+    # Get file paths
+    remote_file = os.path.join(url, audio_path)
+    out_file = os.path.join(out_dir, audio_path)
+
+    # Do nothing if path is a directory
+    if os.path.isdir(out_file):
+        return
+
+    # Make sure the directory exists
+    ensure_dir(os.path.dirname(out_file))
+
+    # Download actual file if doesn't exist yet
+    if not os.path.isfile(out_file):
+        logging.info("Downloading {} into {} ...".format(remote_file, out_file))
+        urllib.urlretrieve(remote_file, out_file)
+
+
+def process(url, data_file, n_jobs, out_dir):
+    """Main process to download the files."""
+    if data_file is None:
+        N = 26
+        return Parallel(n_jobs=n_jobs)(delayed(download_subfolder)(
+            url, i) for i in range(N))
+    else:
+        df = pd.read_csv(data_file, sep="\t", header=None)
+        df.columns = ["size", "audio_path"]
+        for index, row in df.iterrows():
+            download_file(row["audio_path"], url, out_dir)
 
 
 if __name__ == "__main__":
@@ -29,6 +62,18 @@ if __name__ == "__main__":
     parser.add_argument("url",
                         action="store",
                         help="URL where the MSD preview tracks are.")
+    parser.add_argument("-d",
+                        action="store",
+                        dest="data_file",
+                        default=None,
+                        type=str,
+                        help="Path to the file containing the list of all audio files.")
+    parser.add_argument("-o",
+                        action="store",
+                        dest="out_dir",
+                        default="./msd/mp3/",
+                        type=str,
+                        help="Path to the output directory.")
     parser.add_argument("-j",
                         action="store",
                         dest="n_jobs",
@@ -44,7 +89,7 @@ if __name__ == "__main__":
                         level=logging.INFO)
 
     # Call main process
-    process(args.url, args.n_jobs)
+    process(args.url, args.data_file, args.n_jobs, args.out_dir)
 
     # Done!
     logging.info("Done! Took %.2f seconds." % (time.time() - start_time))
